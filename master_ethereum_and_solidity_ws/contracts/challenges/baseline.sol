b2fb19b525e9201928c435a4d218d225493a0404
agustinomarruiz <agustinomarruiz@gmail.com>
2021-12-27 17:45:36 -0300

    Challenge 1 - solution

--- master_ethereum_and_solidity_ws/contracts/challenges/baseline.sol
+++ master_ethereum_and_solidity_ws/contracts/challenges/baseline.sol
@@ -2,7 +2,15 @@
 
 pragma solidity >=0.5.0 <0.9.0;
 
-contract CryptosToken{
-    string name = "Cryptos";
-    uint supply;
+contract CryptosToken {
+    string public constant name = "Cryptos";
+    uint256 supply;
+
+    function getSupply() public view returns (uint256) {
+        return supply;
+    }
+
+    function setSupply(uint256 s) public {
+        supply = s;
+    }
 }
