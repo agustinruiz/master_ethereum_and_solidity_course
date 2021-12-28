85a8dd460486b5d7f8e54fec13b887ea6c814423
agustinomarruiz <agustinomarruiz@gmail.com>
2021-12-28 11:10:44 -0300

    Challenge 4 - solution

---
+++ master_ethereum_and_solidity_ws/contracts/challenges/deposit.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Deposit {
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
