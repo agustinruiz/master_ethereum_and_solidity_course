a660ed45f46b397fccea54e354d5e24de360464f
agustinomarruiz <agustinomarruiz@gmail.com>
2021-12-28 11:57:14 -0300

    Challenge 6 - solution

--- master_ethereum_and_solidity_ws/contracts/challenges/deposit.sol
+++ master_ethereum_and_solidity_ws/contracts/challenges/deposit.sol
@@ -3,13 +3,30 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract Deposit {
+    address public immutable admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    // modifier to check if caller is owner
+    modifier isAdmin() {
+        // If the first argument of 'require' evaluates to 'false', execution terminates and all
+        // changes to the state and to Ether balances are reverted.
+        // This used to consume all gas in old EVM versions, but not anymore.
+        // It is often a good idea to use 'require' to check if functions are called correctly.
+        // As a second argument, you can also provide an explanation about what went wrong.
+        require(msg.sender == admin, "Caller is not admin");
+        _;
+    }
+
     receive() external payable {}
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function transferBalance(address payable recipient) public {
+    function transferBalance(address payable recipient) public isAdmin {
         uint256 balance = getBalance();
         recipient.transfer(balance);
     }
