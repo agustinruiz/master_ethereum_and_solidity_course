85a5aa13daa54013f51973dd780565c37aaa2b5e
agustinomarruiz <agustinomarruiz@gmail.com>
2021-12-28 11:29:24 -0300

    Challenge 5 - solution

--- master_ethereum_and_solidity_ws/contracts/challenges/deposit.sol
+++ master_ethereum_and_solidity_ws/contracts/challenges/deposit.sol
@@ -1,11 +1,16 @@
 //SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 contract Deposit {
     receive() external payable {}
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function transferBalance(address payable recipient) public {
+        uint256 balance = getBalance();
+        recipient.transfer(balance);
     }
 }
